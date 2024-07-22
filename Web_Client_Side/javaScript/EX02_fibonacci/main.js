837b5eab8f76c06053d81168ca72f5ec7c633de8
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-07-22 17:38:09 +0100

    bug corrigido no fibonacci

--- Web_Client_Side/javaScript/EX02_fibonacci/main.js
+++ Web_Client_Side/javaScript/EX02_fibonacci/main.js
@@ -3,6 +3,7 @@ var fim;
 var valor = 0;
 var valor2 = 1;
 var valor3;
+var primeiraPassagem = true
 
 function Fibonacci() {
   inicio = parseInt(document.getElementById("rangeInicio").value);
@@ -10,18 +11,24 @@ function Fibonacci() {
   cont = fim;
   while (cont >= inicio) {
     valor3 = valor + valor2;
-    if (valor == inicio) {
-      document.getElementById("resultado").innerText += " "+ 0;
-    }
-    if(valor == inicio){
-      document.getElementById("resultado").innerText += " "+ 1;
+    if(inicio == 0){
+      if (valor == inicio) {
+        document.getElementById("resultado").innerText += " "+ 0;
+      }
+      if(valor == inicio){
+        document.getElementById("resultado").innerText += " "+ 1;
+      }
+    }else{
+      if(primeiraPassagem){
+        document.getElementById("resultado").innerText += " "+ 1;
+        primeiraPassagem = false
+      }
     }
     for (var i = inicio; i <= fim; i++) {
       if(i == valor3){
         document.getElementById("resultado").innerText += " "+ valor3;
       }
     }
-    
     valor = valor2;
     valor2 = valor3;
     cont -= 1;
