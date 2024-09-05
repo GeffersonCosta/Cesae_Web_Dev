392e460cf50b427b0b5e4dd62772454cee120c1a
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-09-05 20:24:27 +0100

    exe01

--- Web_Server_Side/exeJs/exe01/Js/scriptexe1.js
+++ Web_Server_Side/exeJs/exe01/Js/scriptexe1.js
@@ -8,5 +8,18 @@
 // for (item in nome) {
 //   console.log(nome[item]);
 // }
-var idade = 20;
-
+let listBuy = [];
+var itens;
+function addList() {
+  itens = document.getElementById("itensUser").value;
+  listBuy.push(itens);
+  itens = document.getElementById("itensUser").value = "";
+}
+function showList() {
+  document.getElementById("resultadoTela").innerHTML = " ";
+  for (element in listBuy) {
+    document.getElementById("resultadoTela").innerHTML +=
+      "</br>" + listBuy[element];
+    document.getElementById("resultadoTela").style.fontWeight = "700";
+  }
+}
