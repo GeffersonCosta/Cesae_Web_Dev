a0185099b628c5bf16446057f44cfb4dc643863e
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-09-05 18:25:40 +0100

    exe01

---
+++ Web_Server_Side/exeJs/exe01/script.js
@@ -0,0 +1,12 @@
+// var nome = [];
+// var item = null;
+// item = prompt("adicionar: ");
+// while (item != "fim") {
+//   nome.push(item);
+//   item = prompt("adicionar: ");
+// }
+// for (item in nome) {
+//   console.log(nome[item]);
+// }
+var idade = 20;
+
