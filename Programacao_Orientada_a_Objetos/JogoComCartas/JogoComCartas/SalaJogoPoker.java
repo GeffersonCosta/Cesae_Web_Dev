4bb9c473ab1d66c3c85b6148776d1abae399a61b
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-09-13 22:50:56 +0100

    POO heranca

--- Programacao_Orientada_a_Objetos/JogoComCartas/JogoComCartas/SalaJogoPoker.java
+++ Programacao_Orientada_a_Objetos/JogoComCartas/JogoComCartas/SalaJogoPoker.java
@@ -1,72 +1,101 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class SalaJogoPoker
 {
     int cont=0;
     Jogador jogadorPoker;
     ArrayList<Jogador> listaJogadores;
-    Utilizador utilizadorId;
-    Carta carta = new Carta();
-    ArrayList<Carta> novoBaralho;
+    Carta carta;
+    ArrayList<Carta> novoBaralho = new ArrayList<Carta>();
+    Random rand = new Random();
+    boolean baralhoCheio = false;
     public SalaJogoPoker()
     {
-     
-       listaJogadores = new ArrayList();
-       
+
+        listaJogadores = new ArrayList();
+
     }
-    
+
     void adicionarJogador(){
-        
+
         cont++;
         jogadorPoker = new Jogador();
         jogadorPoker.nomeNoJogo = "Player_"+cont;
         listaJogadores.add(jogadorPoker);
-                
+
     }
+
     void contarJogares(){
-      System.out.println("Existem "+listaJogadores.size()+" Jogares");
+        System.out.println("Existem "+listaJogadores.size()+" Jogares");
     }
-    
-    void mostrarLista(){
+
+    void ListarJogadores(){
         if(listaJogadores.size() != 0){
             for(int i=0; i < listaJogadores.size(); i++){
-            System.out.println("Jogador: "+listaJogadores.get(i).nomeNoJogo);
-        }
+                System.out.println("Jogador: "+listaJogadores.get(i).nomeNoJogo);
+            }
         }
         else{
             System.out.println("Lista vazia");
-            
+
         }
     }
-    
+
     void criarBaralho(){
-        novoBaralho = new ArrayList();
+
         for(int i = 0; i < carta.arrayNaipes.length; i++)
         {
             for(int j = 0; j < carta.arrayRanks.length; j++)
             {
                 novoBaralho.add(new Carta(i,j));
-               System.out.println(novoBaralho.get(j).rank + novoBaralho.get(i).naipe);
             }
         }
-        
+        baralhoCheio = true;
+
     }
-    
+
+    void mostraListaDeBaralho(){
+        if(novoBaralho.size() == 0){
+            System.out.println("Baralho Vazio");
+        }
+        else{
+            for(int i = 0; i < carta.arrayNaipes.length; i++)
+            {
+                for(int j = 0; j < carta.arrayRanks.length; j++)
+                {
+                    System.out.println(novoBaralho.get(i).arrayNaipes[i] + novoBaralho.get(j).rank);
+
+                }
+            }
+        }
+    }
+
+    void numeroDeItensNaLista(){
+        int cont = 0;
+        for(int i = 0; i < novoBaralho.size(); i++)
+        {
+            cont++;
+        }
+        System.out.println(cont);
+    }
+
+
     void flop()
     {
-        for(int i=0; i < 2; i++){
-            for(int j=0; j < 2; j++ ){
-                 
-                 System.out.println(novoBaralho.get(i).rank);
-                
-                 //System.out.println("Saiu o " + carta.rank + " " +  carta.naipe);
+        if(baralhoCheio){
+            for(int i=0; i < 3; i++){
+                int naipeAleatorio = rand.nextInt(3);
+                int rankAleatorio = rand.nextInt(13);
+                System.out.println("Saiu o "+ novoBaralho.get(naipeAleatorio).arrayNaipes[naipeAleatorio] +  " " +  novoBaralho.get(rankAleatorio).rank);
+                novoBaralho.remove(rankAleatorio);
+
             }
         }
-        
-    }    
-    
-    
-    
+        else{
+            System.out.println("Baralho Vazio");
+        }
 
+    }    
     
 }
