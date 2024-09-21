7da059d5b40f95ea1d9a2a8a21aa9723c3fcbb24
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-09-21 16:27:07 +0100

    projeto cofrinho

---
+++ Programacao_Orientada_a_Objetos/Cofrinho/Euro.java
@@ -0,0 +1,21 @@
+
+public class Euro 
+{   
+    double valor;
+    double total;
+    
+  
+    public Euro(){
+             
+    }
+    
+    double converter(double paramValor){
+       return paramValor * 6;
+    }
+
+    
+  
+
+   
+   
+}
