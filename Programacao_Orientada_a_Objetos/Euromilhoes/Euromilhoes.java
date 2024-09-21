056da0160a26e681a7ecb227c621850f6b7ec1bf
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-09-21 16:28:46 +0100

    exe GestaoProduto POO

--- Programacao_Orientada_a_Objetos/Euromilhoes/Euromilhoes.java
+++ Programacao_Orientada_a_Objetos/Euromilhoes/Euromilhoes.java
@@ -3,32 +3,42 @@ import java.util.Random;
 
 public class Euromilhoes
 {
-    ArrayList<Integer> grelhaNumeros = new ArrayList();
-    ArrayList<Integer> grelhaEstrela;
+    ArrayList<Integer> grelhaNumeros = new ArrayList<>();
+    ArrayList<Integer> grelhaEstrela = new ArrayList<>();
     Random numerosAleatorios = new Random();
     public Euromilhoes()
     {
         for(int i = 0; i<50; i++){
             grelhaNumeros.add(i+1);
         }
+        System.out.println();
         System.out.println("Chave do Euromilhões");
         System.out.print("Números: ");
+        
         for(int i=0; i<5; i++){
-           // O codigo dessa forma da ERRO int indexNumeroSorteado = grelhaNumeros.get(numerosAleatorios.nextInt(grelhaNumeros.size()));
+            // indexNumeroSorteado = grelhaNumeros.get(numerosAleatorios.nextInt(grelhaNumeros.size()));
             int indexNumeroSorteado = numerosAleatorios.nextInt(grelhaNumeros.size());
             System.out.print(grelhaNumeros.get(indexNumeroSorteado));
-            
-            
             if(i < 4){
                 System.out.print(",");
             }
             grelhaNumeros.remove(indexNumeroSorteado);
-        }  
-    }
-    void listaNumeros(){
-        for(int i=0; i<grelhaNumeros.size(); i++){
-            System.out.println(grelhaNumeros.get(i));
+        } 
+        
+           for(int i =0; i<12; i++){
+            grelhaEstrela.add(i+1);
         }
+        System.out.println("");
+        System.out.print("Estrelas: ");
+        for(int i=0; i < 2; i++){
+            int indexEstrelaSorteada = numerosAleatorios.nextInt(grelhaEstrela.size());
+            System.out.print(grelhaEstrela.get(indexEstrelaSorteada));
+             if(i < 1){
+                System.out.print(",");
+            }
+            grelhaEstrela.remove(indexEstrelaSorteada);
+        }
+        
     }
-
+   
 }
---
+++ Programacao_Orientada_a_Objetos/GestaoProdutos/Produto.java
@@ -0,0 +1,14 @@
+
+public class Produto
+{
+    String nome;
+    float preco;
+    
+    public Produto(String nome, float preco)
+    {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+   
+}
