1e29a03dc3097b57727773037f901fe19a0ee886
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-09-28 18:39:58 +0100

    update

--- Web_Server_Side/Tarefa-Intermedia/TelaPrincipal/js/script.js
+++ Web_Server_Side/Tarefa-Intermedia/TelaPrincipal/js/script.js
@@ -1,7 +1,5 @@
-var user;
-var resultado;
-function aprendido() {
-  resultado = document.getElementById("resultado");
-  user = prompt("O que mais aprendeu?");
-  resultado.innerHTML += `<li>${user}</li>`;
+var mostrarConteudo;
+function modoIframe() {
+  mostrarConteudo = document.getElementById("main-conteudo")
+  mostrarConteudo.style.display = "block"
 }
