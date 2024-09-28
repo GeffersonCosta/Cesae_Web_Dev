23fa5ffa347ad76880ba745580cad1567f06fb16
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-09-28 21:44:02 +0100

    update

--- Web_Server_Side/Tarefa-Intermedia/exe07/js/script.js
+++ Web_Server_Side/Tarefa-Intermedia/exe07/js/script.js
@@ -1,12 +1,16 @@
 var total = 0;
 var resultadoNaTela;
+arry = [1,2,3,4,5,6,7,8,9]
+console.log(arry)
 function sumArray(arry) {
   for (var i = 0; i < arry.length; i++) {
     total += arry[i];
   }
   console.log(`total: ${total}`);
   resultadoNaTela = document.getElementById("resultadoNaTela");
-  resultadoNaTela.innerText = `Total: ${total}`;
+  resultadoNaTela.innerText = `Array: ${arry} `;
+  resultadoNaTela.innerText += `Total: ${total}`;
   resultadoNaTela.style.fontSize = "1.5rem"
 }
-sumArray([1,2,3,4,5,6,7,8,9]);
+
+sumArray(arry);
