aa107f4a703d1a0fae0ebd9537d0116ba9bf367a
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-10-09 14:05:21 +0100

    pdf kotlin

---
+++ Programacao_Mobile_Android/atividadePraticaLista/exe04/app/src/main/java/com/example/exe04/UserInformationMainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.exe04
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UserInformationMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_user_information_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
